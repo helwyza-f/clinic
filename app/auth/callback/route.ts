7a9eea2883fb3c5c5b0bf3cd144a8ccd94b5d02b
Helwiza Fahry <helwyza@gmail.com>
2026-02-03 21:14:21 +0700

    add callback/route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -4,21 +4,21 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
-  // Ambil parameter 'next' dari URL, default ke dashboard jika tidak ada
+  // Ambil parameter 'next' dari URL, default ke root jika tidak ada
   const next = searchParams.get("next") ?? "/";
 
   if (code) {
     const supabase = await createClient();
 
-    // Tukarkan 'code' menjadi session aktif
+    // Tukarkan 'code' PKCE dari email menjadi session aktif
     const { error } = await supabase.auth.exchangeCodeForSession(code);
 
     if (!error) {
-      // Jika berhasil, arahkan ke halaman ganti password (update-password)
+      // Jika berhasil, arahkan ke halaman tujuan (update-password)
       return NextResponse.redirect(`${origin}${next}`);
     }
   }
 
-  // Jika gagal, kembalikan ke halaman login dengan pesan error
+  // Jika terjadi kegagalan sistem, kembalikan ke login dengan pesan error
   return NextResponse.redirect(`${origin}/auth/login?error=session_invalid`);
 }
