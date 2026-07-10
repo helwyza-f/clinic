b31b2d26f06451445cf5e44e06ceb24271dbea37
Helwiza Fahry <helwyza@gmail.com>
2026-07-10 18:56:06 +0700

    Revert "Remove deprecated @fullcalendar/interaction import and plugin"
    This reverts commit 69fb99d6c9dec8379d54c587de264c5dd45491d1.

--- app/(dashboard)/admin/dashboard_calendar_view.tsx
+++ app/(dashboard)/admin/dashboard_calendar_view.tsx
@@ -2,6 +2,7 @@
 
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
 import { useRouter } from "next/navigation";
 import { EventInput } from "@fullcalendar/core";
 import "./dashboard_calendar.css";
@@ -37,7 +38,7 @@ export default function DashboardCalendarView({
   return (
     <div className="dashboard-fc animate-in fade-in slide-in-from-bottom-4 duration-1000">
       <FullCalendar
-        plugins={[dayGridPlugin]}
+        plugins={[dayGridPlugin, interactionPlugin]}
         initialView="dayGridMonth"
         headerToolbar={{
           left: "prev,next today",
